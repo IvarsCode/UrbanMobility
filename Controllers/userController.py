e2abf20a18a11a9ec343037909653258f963cd2a
IvarsCode <1057848@hr.nl>
2025-06-20 13:01:10 +0200

    oepsiee

--- Controllers/userController.py
+++ Controllers/userController.py
@@ -26,18 +26,121 @@ class UserController:
         elif user["role"] == "ServiceEngineer":
             while True:
                 print("\n--- Service Engineer Menu ---")
-                print("1. Display Users")
-                print("2. Logout")
+                print("1. Update password")
+                print("2. Display Users")
+                print("3. Search for scooter")
+                print("4. Logout")
                 choice = input("Select an option: ").strip()
                 if choice == "1":
-                    self.display_users()
+                    self.update_password()
                 elif choice == "2":
+                    self.display_users()
+                elif choice == "3":
+                    self.search_scooter()
+                elif choice == "4":
                     print("Logging out...")
+                    self.logOut()
                     break
                 else:
                     print("Invalid choice.")
     
-    
+
+    def search_scooter(self):
+        print("=== Search for Scooter ===")
+        print("1. search on Brand")
+        print("2. search on Model")
+        print("3. search on Serial Number")
+        print("4. search on ID")
+        search_choice = input("Select search option: ").strip()
+
+        if search_choice == "1":
+            brand = input("Enter scooter brand: ").strip()
+            self._search_scooter("brand", brand)
+        elif search_choice == "2":
+            model = input("Enter scooter model: ").strip()
+            self._search_scooter("model", model)
+        elif search_choice == "3":
+            serial_number = input("Enter scooter serial number: ").strip()
+            self._search_scooter("serial_number", serial_number)
+        elif search_choice == "4":
+            scooter_id = input("Enter scooter ID: ").strip()
+            self._search_scooter("id", scooter_id)
+        else:
+            print("Invalid search option.")
+
+    def _search_scooter(self, column, value):
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                f"SELECT * FROM scooters WHERE {column}=?",
+                (value,)
+            )
+            scooter = cursor.fetchone()
+
+            if scooter:
+                print(f"Scooter found:"
+                    f"\nID = {scooter[0]}"
+                    f"\nBrand = {scooter[1]}"
+                    f"\nModel = {scooter[2]}"
+                    f"\nSerial Number = {scooter[3]}"
+                    f"\nTop Speed = {scooter[4]} km/h"
+                    f"\nBattery Capacity = {scooter[5]} Wh"
+                    f"\nState of Charge = {scooter[6]}%"
+                    f"\nTarget Range SoC = [{scooter[7]}%, {scooter[8]}%]"
+                    f"\nLocation (lat,long)= ({scooter[9]}, {scooter[10]})"
+                    f"\nOut of Service = {scooter[11]}"
+                    f"\nMileage = {scooter[12]} km"
+                    f"\nLast Maintenance Date = {scooter[13]}"
+                    f"\nIn Service Date = {scooter[14]}")
+            else:
+                print("Scooter not found.")
+
+
+    def update_password(self):
+        print("=== Update Password ===")
+        username = input("Enter your username: ").strip()
+        old_password = input_password("Enter your old password: ").strip()
+        new_password = input_password("Enter your new password: ").strip()
+        confirm_new_password = input_password("Confirm your new password: ").strip()
+
+        if new_password != confirm_new_password:
+            print("New passwords do not match.")
+            return
+
+        hashed_old_password = hash_password(old_password)
+        hashed_new_password = hash_password(new_password)
+
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "SELECT id FROM users WHERE username=? AND password=?",
+                (username, hashed_old_password)
+            )
+            user_id = cursor.fetchone()
+
+            if user_id:
+                cursor.execute(
+                    "UPDATE users SET password=? WHERE id=?",
+                    (hashed_new_password, user_id[0])
+                )
+                conn.commit()
+                print("Password updated successfully.")
+            else:
+                print("Invalid username or old password.")
+        
+    def logOut(self):
+        print("=== Urban Mobility Logout ===")
+        username = input("Enter your username to logout: ").strip()
+
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "UPDATE users SET isLoggedIn = 0 WHERE username = ?",
+                (username,)
+            )
+            conn.commit()
+            print(f"\nUser '{username}' logged out successfully.")
+
     def login(self):
         print("=== Urban Mobility Login ===")
         username = input("Username: ")
@@ -67,7 +170,7 @@ class UserController:
                 print("\nLogin failed. Invalid username or password.")
                 return None
 
-    def add_user(self, addServiceEngineer=False, addSystemAdministrator=False):
+    def add_user(self):
         try:
             username = input("Enter username: ").strip()
             password = input_password("Enter password: ").strip()
--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 from Models.db import init_db
-
+from Utils import *
 from Controllers.userController import UserController
-
+#insert_dummy_scooters(50)  #run to run
 
 def main():
     print("Welcome to Urban Mobility Backend System")
     init_db()
+     
     
     controller = UserController()  
-    controller.display_users()
-    controller.add_user()
+    
+    #controller.display_users()
+    #controller.add_user()
 
     user = None
     while not user:
         user = controller.login()
+        controller.show_menu(user)
 
-    # Proceed to the menu based on role
-    if user["role"] == "ServiceEngineer":
-        print("Loading Service Engineer Menu...")
-        # service_engineer_menu(user)
-    elif user["role"] == "SystemAdministrator":
-        print("Loading System Administrator Menu...")
-        # system_admin_menu(user)
-    elif user["role"] == "SuperAdministrator":
-        print("Loading Super Administrator Menu...")
-        # super_admin_menu(user)
+    
 
 
 if __name__ == "__main__":
